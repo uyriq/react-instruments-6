75a27c28084a60583cebb857e57837f150f582ac
uyriq <105006434+uyriq@users.noreply.github.com>
2022-05-18 15:39:48 +0300

    Update header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import headerLogo from '../../images/logo.png';
 
 class Header extends React.Component {
   render() {
     return (
       <header>
-      <h1 >Собачки, следящие за тобой, пока ты изучаешь React</h1>
-    </header>
-    );
+        <img src={headerLogo} alt="логотип." />
+        <h1>Собачки, следящие за тобой, пока ты изучаешь React</h1>
+      </header>
+    );              
   }
 }
+
 export default Header;

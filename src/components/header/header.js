8353f152811dbb7e865e2ada1678e5aa0cd3f11b
uyriq <105006434+uyriq@users.noreply.github.com>
2022-05-18 16:05:08 +0300

    headerStyles

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import headerLogo from '../../images/logo.png';
+import headerStyles from './header.module.css';
 
 class Header extends React.Component {
   render() {
     return (
-      <header>
-        <img src={headerLogo} alt="логотип." />
-        <h1>Собачки, следящие за тобой, пока ты изучаешь React</h1>
+      <header className={headerStyles.header}>
+        <img src={headerLogo} alt="логотип." className={headerStyles.logo} />
+        <h1 className={headerStyles.title}>Собачки, следящие за тобой, пока ты изучаешь React</h1>
       </header>
     );              
   }

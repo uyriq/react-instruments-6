a886e405e5da3587575960f0e6c24865d88f4b52
uyriq <105006434+uyriq@users.noreply.github.com>
2022-05-18 16:22:50 +0300

    styles-2

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,10 +7,17 @@ import Main from '../main/main';
 
 class App extends React.Component {
   render() {
+    const mainData = {
+      title: "Группа поддержки",
+      description: "Это самые преданные болельщики, которые будут радоваться твоим успехам и грустить вместе с тобой в тяжёлые времена",
+      caption: "Нужно не забыть покормить их",
+      image: mainImage
+    }
+
     return (
       <div className={appStyles.app}>
         <Header />
-        <Main image={mainImage} />
+        <Main mainData={mainData} />
       </div>
     );
   }
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import mainStyles from './main.module.css'
 
 class Main extends React.Component {
   render() {
+    const { image, caption, title, description } = this.props.mainData;
+    
     return (
-      <main>
-        <img src={this.props.image} alt="фото собачек." />
+      <main className={mainStyles.main}>
+        <img src={image} alt="фото собачек." className={mainStyles.image} />
+        <span className={mainStyles.caption}>{caption}</span>
+        <h2 className={mainStyles.title}>{title}</h2>
+        <p className={mainStyles.description}>{description}</p>
       </main>
     );
   }
